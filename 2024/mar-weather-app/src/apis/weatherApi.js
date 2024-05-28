4d175c563971a6eaec8e20b4c02867f0579b562c
bino kochumol varghese <binomathewvarghese@gmail.com>
2024-05-28 09:25:51 +0300

    useMemo app created for blog.

--- 2024/mar-weather-app/src/apis/weatherApi.js
+++ 2024/mar-weather-app/src/apis/weatherApi.js
@@ -9,5 +9,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "538066005cac57d0632757b6c24dd56f";
+export const WEATHER_API_KEY = "xx";
 export const WEATHER_API_IMG  = "https://openweathermap.org/img/wn/"
